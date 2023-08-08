6fd11cd32a254db438e6369494c8db496c5ac5dd
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-08-08 23:37:53 +0300

    Add API functionality

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     super();
     this.state = {
       input: "",
+      // IMAGE_URL: "",
     };
   }
 
@@ -18,8 +19,55 @@ class App extends React.Component {
   };
 
   onButtonClick = () => {
+    // this.setState({ input: this.state.input });
     console.log("button clicked");
+    // const inputValue = this.state.input;
+    // this.setState({
+    //   IMAGE_URL: inputValue,
+    // });
+
+    // console.log(this.state.IMAGE_URL);
+    const PAT = "f447eee4fd174578bfce293b901e2fe7";
+    const USER_ID = "john23";
+    const APP_ID = "testFace";
+    const MODEL_ID = "face-detection";
+    const IMAGE_URL =
+      "https://upload.wikimedia.org/wikipedia/commons/3/3f/Baloch_people_%284%29.jpg";
+
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: IMAGE_URL,
+            },
+          },
+        },
+      ],
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key " + PAT,
+      },
+      body: raw,
+    };
+
+    fetch(
+      "https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("API error:", error));
   };
+
   render() {
     return (
       <>
