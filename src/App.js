45d05f26302c1a0563f6bc89dd55f6155f0ab2d1
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-08-08 12:28:03 +0300

    Update input and button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
+import React from "react";
 import "./App.css";
 import Logo from "./components/Logo/Logo";
 import Navigation from "./components/Navigation/Navigation";
 import ImgLinkForm from "./components/ImgLinkForm/ImgLinkForm";
 import Rank from "./components/Rank/Rank";
 
-function App() {
-  return (
-    <>
-      <section className="mainNav">
-        <Logo />
-        <Navigation />
-      </section>
-      <section className="mainSection">
-        <Rank />
-        <ImgLinkForm />
-      </section>
-    </>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonClick = () => {
+    console.log("button clicked");
+  };
+  render() {
+    return (
+      <>
+        <section className="mainNav">
+          <Logo />
+          <Navigation />
+        </section>
+        <section className="mainSection">
+          <Rank />
+          <ImgLinkForm
+            onInputChange={this.onInputChange}
+            onButtonClick={this.onButtonClick}
+          />
+        </section>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/ImgLinkForm/ImgLinkForm.js
+++ src/components/ImgLinkForm/ImgLinkForm.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const ImgLinkForm = () => {
+const ImgLinkForm = ({ onInputChange, onButtonClick }) => {
   return (
     <>
       <p>{"Enter the URL of an image in the box below"}</p>
       <div>
-        <input type="text" />
-        <button>Submit</button>
+        <input type="text" onChange={onInputChange} />
+        <button onClick={onButtonClick}>Submit</button>
       </div>
     </>
   );
