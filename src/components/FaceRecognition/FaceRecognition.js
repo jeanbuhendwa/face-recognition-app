be91f4442207bb9fd2d0f18a6e70b5c2bdd57a6e
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-08-12 21:54:13 +0300

    Add face box

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,25 @@
 import React from "react";
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({ IMAGE_URL }) => {
+const FaceRecognition = ({ IMAGE_URL, box }) => {
   return (
     <>
-      <img src={IMAGE_URL} alt="face" id="face" width="400px" height="auto" />
+      <img
+        src={IMAGE_URL}
+        alt="face"
+        id="inputImage"
+        width="500px"
+        height="auto"
+      />
+      <div
+        className="bounding-box"
+        style={{
+          top: box.topRow,
+          right: box.rightCol,
+          bottom: box.bottomRow,
+          left: box.leftCol,
+        }}
+      ></div>
     </>
   );
 };
